ac29229d75f3b1f970b185958b3ea1d33bc8c041
Manu <bilbaomanuel98@gmail.com>
2020-05-24 23:34:19 -0300

    Mensajede satisfaccion/error al enviar contacto

--- static/js/contacto.js
+++ static/js/contacto.js
@@ -1,8 +1,40 @@
-$("#contactForm").submit(function(e) {
-	e.preventDefault();
+$(function() {
+	$("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+		preventSubmit: true,
 
-	var $form = $(this);
-	$.post($form.attr("action"), $form.serialize()).then(function() {
-		alert("Gracias!");
+		submitSuccess: function($form, event) {
+			event.preventDefault();
+
+			var nombre = $("input#nombre").val();
+			var mail = $("input#mail").val();
+			var telefono = $("input#telefono").val();
+			var mensaje = $("textarea#mensaje").val();
+
+			var btn = $("#btnEnviarMensaje");
+			btn.prop("disabled", true);
+
+			$.post($form.attr("action"), $form.serialize())
+				.done(function() {
+					$('#contactStatus').html("<div class='alert alert-success'>");
+					$('#contactStatus > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>")
+					$('#contactStatus > .alert-success')
+						.append("<strong>Su mensaje fue enviado. Le responderemos a la brevedad</strong>");
+					$('#contactStatus > .alert-success').append('</div>');
+					//clear all fields
+					$('#contactForm').trigger("reset");
+				})
+				.fail(function() {
+					$('#contactStatus').html("<div class='alert alert-danger'>");
+					$('#contactStatus > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+					$('#contactStatus > .alert-danger')
+						.append("<strong>No se pudo enviar el mensaje. Por favor intente nuevamente");
+					$('#contactStatus > .alert-danger').append('</div>');
+				})
+				.always(function() {
+					setTimeout(function() {
+						btn.prop("disabled", false)
+					}, 3000);
+				});
+		},
 	});
 });
