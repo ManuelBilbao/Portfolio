66111f42452ee552c0039ae7614a0da9eae429c6
Manu <bilbaomanuel98@gmail.com>
2020-05-30 00:26:40 -0300

    Falto este archivo al commit anterior

--- plantillas/servicios.php
+++ plantillas/servicios.php
@@ -5,6 +5,16 @@
 	<?php require("head.php") ?>
 	<link rel="stylesheet" href="/static/css/servicios.css">
 	<link rel="stylesheet" href="/static/css/carousel.css">
+
+	<style>
+		@media (max-width: 992px) {
+
+			.caracteristicas,
+			.caracteristicas div {
+				text-align: center!important;
+			}
+		}
+	</style>
 </head>
 <body id="page-top">
 	<!-- Navigation -->
@@ -20,15 +30,19 @@
 					<li class="nav-item">
 						<a href="/" class="nav-link">Inicio</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger" href="#caracteristicas">Características</a>
-					</li>
+					<?php if (isset($caracteristicas)): ?>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#caracteristicas">Características</a>
+						</li>
+					<?php endif ?>
 					<li class="nav-item">
 						<a class="nav-link js-scroll-trigger" href="#prestaciones">Prestaciones</a>
 					</li>
-					<li class="nav-item">
-						<a class="nav-link js-scroll-trigger" href="#precios">Precios</a>
-					</li>
+					<?php if (isset($precios)): ?>
+						<li class="nav-item">
+							<a class="nav-link js-scroll-trigger" href="#precios">Precios</a>
+						</li>
+					<?php endif ?>
 				</ul>
 			</div>
 		</div>
@@ -40,60 +54,68 @@
 		<h1 class="header-title"><?php echo $nombre ?></h1>
 	</header>
 
-	<section class="page-section" id="caracteristicas">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<h2 class="section-heading text-uppercase">Características</h2>
-				</div>
-			</div>
-			<div class="row mt-3 text-justify">
-				<div class="col-12 col-md-10 col-lg-8 mx-auto">
-					<p class="text-muted">
-						<strong><em>Estilo ñ</em></strong> es un estudio de desarrollo web dispuesto a desafiarse en todo momento y elaborar proyectos que sobresalgan, ofreciendo soluciones personalizadas para las necesidades de todos los clientes.
-						<br>
-						Creemos que lo más importante para una empresa es la imagen y la seguridad que transmite a sus clientes y es por eso que nosotros nos enfocamos en los detalles para cubrir al máximo todos los aspectos de un sitio web.
-					</p>
-					<p class="text-muted">
-						En <strong><em>Estilo ñ</em></strong> lo estaremos acompañando en todo el proceso de esta nueva etapa, desde el minuto cero aconsejándolo para que la transición hacia su nuevo sitio sea el ideal y libre de cualquier duda.
-					</p>
-					<p class="text-muted">
-						Bienvenido a <strong><em>Estilo ñ</em></strong>, donde el verdadero cambio comienza y los sueños se hacen realidad.
-					</p>
-				</div>
-			</div>
-		</div>
-	</section>
+	<?php if (isset($caracteristicas)): ?>
+		<div id="caracteristicas"></div>
+		<?php foreach ($caracteristicas as $n => $caracteristica): ?>
+			<?php $right = $n % 2 ?>
 
-	<section class="page-section bg-light" id="precios">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<h2 class="section-heading text-uppercase">Precios</h2>
-				</div>
-			</div>
+			<?php if ($n > 0): ?><hr class="w-50"><?php endif ?>
 
-			<div class="row d-none d-lg-flex mt-3">
-				<?php foreach ($precios as $precio): ?>
-					<div class="col">
-						<?php include("servicios/card-precios.php"); ?>
-					</div>
-				<?php endforeach ?>
-			</div>
-
-			<div class="row d-flex d-lg-none">
-				<div class="col-12">
-					<div class="mat-carousel">
-						<?php foreach ($precios as $precio): ?>
-							<div class="mat-carousel-item w-75">
-								<?php include("servicios/card-precios.php"); ?>
+			<section class="page-section caracteristicas">
+				<div class="container">
+					<div class="row">
+						<?php if ($right): ?>
+							<div class="col-12 col-lg-6 order-1 order-lg-12 align-self-center">
+						<?php else: ?>
+							<div class="col-12 col-lg-6 order-12 order-lg-1 align-self-center">
+						<?php endif ?>
+								<h2 class="section-heading"><?php echo $caracteristica["titulo"] . $left ?></h2>
+								<p class="text-muted"><?php echo $caracteristica["descripcion"] ?></p>
 							</div>
-						<?php endforeach ?>
+						<?php if ($right): ?>
+							<div class="col-12 col-lg-6 order-12 order-lg-1 mb-4 mb-lg-0">
+						<?php else: ?>
+							<div class="col-12 col-lg-6 order-1 order-lg-12 mb-4 mb-lg-0 text-right">
+						<?php endif ?>
+								<img src="/static/img/servicios/<?php echo $caracteristica["imagen"] ?>" alt="" class="w-75">
+							</div>
+					</div>
+				</div>
+			</section>
+		<?php endforeach ?>
+	<?php endif ?>
+
+	<?php if (isset($precios)): ?>
+		<section class="page-section bg-light" id="precios">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 text-center">
+						<h2 class="section-heading text-uppercase">Precios</h2>
+					</div>
+				</div>
+
+				<div class="row d-none d-lg-flex mt-3">
+					<?php foreach ($precios as $precio): ?>
+						<div class="col">
+							<?php include("servicios/card-precios.php"); ?>
+						</div>
+					<?php endforeach ?>
+				</div>
+
+				<div class="row d-flex d-lg-none">
+					<div class="col-12">
+						<div class="mat-carousel">
+							<?php foreach ($precios as $precio): ?>
+								<div class="mat-carousel-item w-75">
+									<?php include("servicios/card-precios.php"); ?>
+								</div>
+							<?php endforeach ?>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
+	<?php endif ?>
 
 	<?php require("footer.php") ?>
 
