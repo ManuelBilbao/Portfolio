1a4c022d9721e028c09a5734495a9d53846f5004
Manu <bilbaomanuel98@gmail.com>
2020-05-30 18:21:24 -0300

    Caracteristicas de turnos

--- turnos/index.php
+++ turnos/index.php
@@ -3,6 +3,22 @@
 $nombre = "Turnos";
 $header_background = "turnos/header.jpg";
 
+$caracteristicas = [
+	[
+		"titulo" => "Turnos online",
+		"descripcion" => "Adquiera un sistema interno de turnos con el cual podrá organizar eficazmente sus horarios y los de su empresa, o bien brindarle a sus clientes la posibilidad de registrar sus propias citas a través de su sitio.",
+		"imagen" => "turnos/1.png"
+	], [
+		"titulo" => "Servicios de salud",
+		"descripcion" => "Escencial para centros de salud, consultorios médicos y terapéuticos que quieran sumar pacientes mediante la facilitación de los turnos vía web.",
+		"imagen" => "turnos/2.png"
+	], [
+		"titulo" => "Centros deportivos",
+		"descripcion" => "Un sistema ideal para reservar plazas, clases y canchas, con la posibilidad de suscripciones periódicas y opciones de pago por adelantado.",
+		"imagen" => "turnos/3.png",
+	]
+];
+
 require("../plantillas/servicios.php");
 
 ?>
