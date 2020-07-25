d35f9605ac4ffa77b5f35331a874febc68dafca4
Manuel Bilbao (Lomas) <bilbaomanuel98@gmail.com>
2020-07-25 18:46:01 -0300

    Agregado 'mas...' a otros

--- otros/index.php
+++ otros/index.php
@@ -103,6 +103,17 @@
 						</div>
 					</div>
 				</div>
+				<div class="col-12 col-md-6 mb-3">
+					<div class="row">
+						<div class="col-9 col-lg-4 mx-auto">
+							<img src="/static/img/servicios/otros/mas.png" alt="Mucho más" class="w-100">
+						</div>
+						<div class="col-lg servicio">
+							<h3 class="section-heading">Mucho más...</h3>
+							<p class="text-muted">Le ofrecemos soluciones personalizadas exclusivamente para sus necesidades, no dude en consultarnos.</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -128,7 +139,7 @@
 					</button>
 				</div>
 				<form name="presupuestoForm" netlify>
-					<input type="hidden" class="hidden" name="servicio" value="<?php echo $nombre ?>">
+					<input type="hidden" class="hidden" name="servicio" value="Otros">
 					<div class="modal-body">
 						<div class="container-fluid">
 							<div class="row">
